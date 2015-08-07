7f1906fd666aedb5a58e805148b6c7ebb4c32fd4
Martin Journois <martin.journois@fr.michelin-solutions.com>
2015-08-07 18:21:29 +0200

    add Popup in Markers

--- folium/plugins/marker.py
+++ folium/plugins/marker.py
@@ -5,9 +5,31 @@ Marker plugin
 
 """
 from jinja2 import Template
-#import json
+import json
 
 from .plugin import Plugin
+
+class Popup(Plugin):
+    def __init__(self, html, max_width=300):
+        super(Popup, self).__init__()
+        self.plugin_name = 'Popup'
+        self.html = json.dumps(html)
+        self.max_width = max_width
+        self.map = None
+
+    def render(self, **kwargs):
+        assert self.map is not None, "Cannot render a Popup that have no 'map' attribute."
+        #{{ pop_name }}.bindPopup({{ pop_txt }});
+        #{{ pop_name }}._popup.options.maxWidth = {{ width }};
+        self.map.figure.script['Popup_'+self.object_name] = Template("""
+        var Popup_{id} = L.popup({{
+            maxWidth: '{maxWidth}'
+            }}).setContent({html});""".format(
+                html=self.html,
+                maxWidth=self.max_width,
+                id=self.object_name,
+                ))
+        return 'Popup_'+self.object_name
 
 class Icon(Plugin):
     def __init__(self, color='blue', icon='info-sign', angle=0):
@@ -17,8 +39,8 @@ class Icon(Plugin):
         self.icon = icon
         self.angle = angle
         self.map = None
-        
-    def render(self):
+
+    def render(self, **kwargs):
         assert self.map is not None, "Cannot render an Icon that have no 'map' attribute."
         self.map.figure.script['Icon_'+self.object_name] = Template("""
         var Icon_{id} = L.AwesomeMarkers.icon({{
@@ -65,18 +87,21 @@ class Marker(Plugin):
         self.plugin_name = 'Marker'
         self.location = location
         self.icon = Template("new L.Icon.Default()") if icon is None else icon
+        self.popup = Template("") if popup is None else popup
 
     def add_to_map(self, map):
         """Adds the plugin on a folium.map object."""
         super(Marker, self).add_to_map(map)
         self.icon.map = map
+        self.popup.map = map
 
     def render_js(self, nb):
         return """
-        var Marker_{id} = L.marker([{location[0]},{location[1]}], {{icon:{icon}}});
+        var Marker_{id} = L.marker([{location[0]},{location[1]}], {{icon:{icon}}}){popup};
         map.addLayer(Marker_{id});
         """.format(
             id=self.object_name,
             location = self.location,
             icon = self.icon.render(),
+            popup = ".bindPopup({})".format(self.popup.render()),
             )
