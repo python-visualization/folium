0fa334a546bf157025a6d9f45ba3af1c64aab7c9
Martin Journois <martin.journois@fr.michelin-solutions.com>
2015-08-25 17:27:19 +0200

    Set default header links in Figure.__init__

--- folium/features.py
+++ folium/features.py
@@ -100,6 +100,70 @@ class Figure(Feature):
             {{this.script.render(**kwargs)}}
         </script>
         """)
+
+        # Create the meta tag
+        self.header.add_children(Feature(
+            '<meta http-equiv="content-type" content="text/html; charset=UTF-8" />'),
+                                  name='meta_http')
+
+        # Import Javascripts
+        for name, url in [
+            ('leaflet',
+                 "https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.3/leaflet.js"),
+            ('jquery',
+                 "https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"),
+            ('bootstrap',
+                 "https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"),
+            ('awesome_markers',
+                 "https://rawgithub.com/lvoogdt/Leaflet.awesome-markers/2.0/develop/dist/leaflet.awesome-markers.js"),
+            ('marker_cluster_src',
+                 "https://cdnjs.cloudflare.com/ajax/libs/leaflet.markercluster/0.4.0/leaflet.markercluster-src.js"),
+            ('marker_cluster',
+                 "https://cdnjs.cloudflare.com/ajax/libs/leaflet.markercluster/0.4.0/leaflet.markercluster.js"),
+            ]:
+            self.header.add_children(JavascriptLink(url), name=name)
+
+        # Import Css
+        for name, url in [
+            ("leaflet_css",
+                 "https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.3/leaflet.css"),
+            ("bootstrap_css",
+                 "https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css"),
+            ("bootstrap_theme_css",
+                 "https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap-theme.min.css"),
+            ("awesome_markers_font_css",
+                 "https://maxcdn.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css"),
+            ("awesome_markers_css",
+                 "https://rawgit.com/lvoogdt/Leaflet.awesome-markers/2.0/develop/dist/leaflet.awesome-markers.css"),
+            ("marker_cluster_default_css",
+                 "https://cdnjs.cloudflare.com/ajax/libs/leaflet.markercluster/0.4.0/MarkerCluster.Default.css"),
+            ("marker_cluster_css",
+                 "https://cdnjs.cloudflare.com/ajax/libs/leaflet.markercluster/0.4.0/MarkerCluster.css"),
+            ("awesome_rotate_css",
+                 "https://raw.githubusercontent.com/python-visualization/folium/master/folium/templates/leaflet.awesome.rotate.css"),
+            ]:
+            self.header.add_children(CssLink(url), name=name)
+
+        self.header.add_children(Feature("""
+            <style>
+
+            html, body {
+                width: 100%;
+                height: 100%;
+                margin: 0;
+                padding: 0;
+                }
+
+            #map {
+                position:absolute;
+                top:0;
+                bottom:0;
+                right:0;
+                left:0;
+                }
+            </style>
+            """), name='css_style')
+
     def to_dict(self, depth=-1, **kwargs):
         out = super(Figure, self).to_dict(depth=-1, **kwargs)
         out['header'] = self.header.to_dict(depth=depth-1, **kwargs)
