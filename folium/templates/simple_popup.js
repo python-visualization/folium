b7ae4cc37b30b2d081c872577171d53b0136ac42
Martin Journois <martin.journois@fr.michelin-solutions.com>
2015-09-07 18:30:40 +0200

    cleanup simple_popup.js

--- folium/templates/simple_popup.js
+++ folium/templates/simple_popup.js
@@ -1,6 +1,4 @@
 var {{ pop_name }} = L.popup({maxWidth: '{{ width }}'});
 var {{ html_name }} =
-    $('<div id="{{ html_name }}" style="width: 100.0%; height: 100.0%;">
-          {{ pop_txt }}
-          </div>')[0];
+    $('<div id="{{ html_name }}" style="width: 100.0%; height: 100.0%;">{{ pop_txt }}</div>')[0];
 {{ pop_name }}.setContent({{ html_name }});
