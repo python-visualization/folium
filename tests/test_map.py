6cc370b834584e736f0cd8ca4e5a6af2350bbec1
ocefpaf <ocefpaf@gmail.com>
2015-11-16 16:54:56 -0300

    Popup tests.

---
+++ tests/test_map.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+
+"""
+Folium map Tests
+----------------
+
+"""
+from __future__ import unicode_literals
+
+from folium.map import Popup  # TODO: Map, Marker, ...
+
+
+tmpl = """
+        <div id="{id}"
+                style="width: {width}; height: {height};">
+                {text}</div>
+""".format
+
+
+def test_popup_ascii():
+    popup = Popup('Some text.')
+    _id = list(popup.html._children.keys())[0]
+    kw = dict(id=_id,
+              width='100.0%',
+              height='100.0%',
+              text='Some text.')
+    assert popup.html.render().strip() == tmpl(**kw).strip()
+
+
+def test_popup_quotes():
+    popup = Popup("Let's try quotes")
+    _id = list(popup.html._children.keys())[0]
+    kw = dict(id=_id,
+              width='100.0%',
+              height='100.0%',
+              text='Let&#39;s try quotes')
+    assert popup.html.render().strip() == tmpl(**kw).strip()
+
+
+def test_popup_unicode():
+    popup = Popup("Ça c'est chouette")
+    _id = list(popup.html._children.keys())[0]
+    kw = dict(id=_id,
+              width='100.0%',
+              height='100.0%',
+              text="Ça c&#39;est chouette")
+    assert popup.html.render().strip() == tmpl(**kw).strip()
