c14160956f1de0603f7bc4559af3a1233a7da84f
Martin Journois <martin.journois@fr.michelin-solutions.com>
2016-03-22 07:43:02 +0100

    Add branca imports in __init__

--- folium/__init__.py
+++ folium/__init__.py
@@ -2,11 +2,14 @@
 
 from __future__ import absolute_import
 
-from folium.folium import Map, initialize_notebook, CircleMarker
+from branca.element import (CssLink, Div, Element, Figure, Html, IFrame,
+                            JavascriptLink, Link, MacroElement)
+from branca.colormap import (ColorMap, LinearColormap, StepColormap)
 
+
+from folium.folium import Map, initialize_notebook, CircleMarker
 from folium.map import (FeatureGroup, FitBounds, Icon, LayerControl, Marker,
                         Popup, TileLayer)
-
 from folium.features import (ClickForMarker, CustomIcon, DivIcon,
                              GeoJson, LatLngPopup,
                              MarkerCluster, MultiPolyLine, PolyLine, Vega,
@@ -14,7 +17,19 @@ from folium.features import (ClickForMarker, CustomIcon, DivIcon,
 
 __version__ = '0.3.0.dev'
 
-__all__ = ['Map',
+__all__ = ['CssLink',
+           'Div',
+           'Element',
+           'Figure',
+           'Html',
+           'IFrame',
+           'JavascriptLink',
+           'Link',
+           'MacroElement',
+           'ColorMap',
+           'LinearColormap',
+           'StepColormap',
+           'Map',
            'initialize_notebook',
            'CircleMarker',
            'FeatureGroup',
