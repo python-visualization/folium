561e86df5d4dfdb245820943bbb7c349d0f8c006
George Harris <glfharris@gmail.com>
2016-06-28 19:13:46 +0100

    Fix PEP8 formatting

--- examples/choropleth_counties.py
+++ examples/choropleth_counties.py
@@ -33,30 +33,30 @@ df.dropna(subset=['GEO_ID'], inplace=True)
 
 # Number of employed with auto scale.
 map_1 = folium.Map(location=[48, -102], zoom_start=3)
-map_1.geo_json(geo_path=county_geo, data_out='data1.json', data=df,
-               columns=['GEO_ID', 'Employed_2011'],
-               key_on='feature.id',
-               fill_color='YlOrRd', fill_opacity=0.7, line_opacity=0.3,
-               topojson='objects.us_counties_20m')
+map_1.choropleth(geo_path=county_geo, data_out='data1.json', data=df,
+                 columns=['GEO_ID', 'Employed_2011'],
+                 key_on='feature.id',
+                 fill_color='YlOrRd', fill_opacity=0.7, line_opacity=0.3,
+                 topojson='objects.us_counties_20m')
 map_1.save(outfile='map_1.html')
 
 # Unemployment with custom defined scale.
 map_2 = folium.Map(location=[40, -99], zoom_start=4)
-map_2.geo_json(geo_path=county_geo, data_out='data2.json', data=df,
-               columns=['GEO_ID', 'Unemployment_rate_2011'],
-               key_on='feature.id',
-               threshold_scale=[0, 5, 7, 9, 11, 13],
-               fill_color='YlGnBu', line_opacity=0.3,
-               legend_name='Unemployment Rate 2011 (%)',
-               topojson='objects.us_counties_20m')
+map_2.choropleth(geo_path=county_geo, data_out='data2.json', data=df,
+                 columns=['GEO_ID', 'Unemployment_rate_2011'],
+                 key_on='feature.id',
+                 threshold_scale=[0, 5, 7, 9, 11, 13],
+                 fill_color='YlGnBu', line_opacity=0.3,
+                 legend_name='Unemployment Rate 2011 (%)',
+                 topojson='objects.us_counties_20m')
 map_2.save(outfile='map_2.html')
 
 # Median Household income.
 map_3 = folium.Map(location=[40, -99], zoom_start=4)
-map_3.geo_json(geo_path=county_geo, data_out='data3.json', data=df,
-               columns=['GEO_ID', 'Median_Household_Income_2011'],
-               key_on='feature.id',
-               fill_color='PuRd', line_opacity=0.3,
-               legend_name='Median Household Income 2011 ($)',
-               topojson='objects.us_counties_20m')
+map_3.choropleth(geo_path=county_geo, data_out='data3.json', data=df,
+                 columns=['GEO_ID', 'Median_Household_Income_2011'],
+                 key_on='feature.id',
+                 fill_color='PuRd', line_opacity=0.3,
+                 legend_name='Median Household Income 2011 ($)',
+                 topojson='objects.us_counties_20m')
 map_3.save(outfile='map_3.html')
