fcadba13b18eae35a2271d28d2a6f88d8413eaad
palewire <ben.welsh@gmail.com>
2016-12-31 11:52:34 -0800

    Added weight to circle_marker.js

--- folium/templates/circle_marker.js
+++ folium/templates/circle_marker.js
@@ -1,5 +1,6 @@
 var {{ circle }} = L.circleMarker([{{ lat }}, {{ lon }}], {
                             color: '{{ line_color }}',
                             fillColor: '{{ fill_color }}',
+                            weight: '{{ weight }}',
                             fillOpacity: {{ fill_opacity }}
                             }).setRadius({{ radius }})
