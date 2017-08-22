95d97e591a85cca7263a3777b5c450ad63ce2468
Filipe Fernandes <ocefpaf@gmail.com>
2017-08-22 11:42:14 -0300

    video overlay

---
+++ folium/plugins/video_overlay.py
@@ -0,0 +1,60 @@
+"""
+Video Overlay
+-------------
+
+Used to load and display a video over the map.
+
+"""
+
+import json
+
+from folium.map import Layer
+
+from jinja2 import Template
+
+
+class VideoOverlay(Layer):
+    def __init__(self, video_url, bounds, opacity=1., attr=None,
+                 autoplay=True, loop=True):
+        """
+        Used to load and display a video over the map.
+
+        Parameters
+        ----------
+        video_url: URL of the video
+        bounds: list
+            Video bounds on the map in the form [[lat_min, lon_min],
+            [lat_max, lon_max]]
+        opacity: float, default Leaflet's default (1.0)
+        attr: string, default Leaflet's default ('')
+
+        """
+        super(VideoOverlay, self).__init__()
+        self._name = 'VideoOverlay'
+
+        self.video_url = video_url
+
+        self.bounds = json.loads(json.dumps(bounds))
+        options = {
+            'opacity': opacity,
+            'attribution': attr,
+            'loop': loop,
+            'autoplay': autoplay,
+        }
+        self.options = json.dumps(options)
+
+        self._template = Template(u"""
+            {% macro script(this, kwargs) %}
+                var {{this.get_name()}} = L.videoOverlay(
+                    '{{ this.video_url }}',
+                    {{ this.bounds }},
+                    {{ this.options }}
+                    ).addTo({{this._parent.get_name()}});
+            {% endmacro %}
+            """)
+
+    def _get_self_bounds(self):
+        """Computes the bounds of the object itself (not including it's children)
+        in the form [[lat_min, lon_min], [lat_max, lon_max]]
+        """
+        return self.bounds
