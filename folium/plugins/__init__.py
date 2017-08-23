4ff6a8c3e67389f7ba7d0640d6ffe1e9fba4da59
Filipe Fernandes <ocefpaf@gmail.com>
2017-08-23 17:33:48 -0300

    Implement measure plugin closes #472

--- folium/plugins/__init__.py
+++ folium/plugins/__init__.py
@@ -13,6 +13,7 @@ from .fullscreen import Fullscreen
 from .heat_map import HeatMap
 from .image_overlay import ImageOverlay
 from .marker_cluster import MarkerCluster
+from .measure_control import MeasureControl
 from .polyline_text_path import PolyLineTextPath
 from .scroll_zoom_toggler import ScrollZoomToggler
 from .terminator import Terminator
@@ -20,16 +21,17 @@ from .timestamped_geo_json import TimestampedGeoJson
 from .timestamped_wmstilelayer import TimestampedWmsTileLayers
 
 __all__ = [
-    'MarkerCluster',
-    'FastMarkerCluster',
-    'ScrollZoomToggler',
-    'Terminator',
     'BoatMarker',
-    'TimestampedGeoJson',
+    'FastMarkerCluster',
+    'FloatImage',
+    'Fullscreen',
     'HeatMap',
     'ImageOverlay',
-    'Fullscreen',
+    'MarkerCluster',
+    'MeasureControl',
     'PolyLineTextPath',
-    'FloatImage',
+    'ScrollZoomToggler',
+    'Terminator',
+    'TimestampedGeoJson',
     'TimestampedWmsTileLayers'
     ]
