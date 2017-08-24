649dbcbb3cbac9f529330379fcd89d5419b029e0
Filipe Fernandes <ocefpaf@gmail.com>
2017-08-24 19:36:05 -0300

    absolute imports

--- folium/plugins/__init__.py
+++ folium/plugins/__init__.py
@@ -7,19 +7,19 @@ Add different objects/effects on a folium map.
 
 from __future__ import (absolute_import, division, print_function)
 
-from .boat_marker import BoatMarker
-from .fast_marker_cluster import FastMarkerCluster
-from .float_image import FloatImage
-from .fullscreen import Fullscreen
-from .heat_map import HeatMap
-from .image_overlay import ImageOverlay
-from .marker_cluster import MarkerCluster
-from .measure_control import MeasureControl
-from .polyline_text_path import PolyLineTextPath
-from .scroll_zoom_toggler import ScrollZoomToggler
-from .terminator import Terminator
-from .timestamped_geo_json import TimestampedGeoJson
-from .timestamped_wmstilelayer import TimestampedWmsTileLayers
+from folium.plugins.boat_marker import BoatMarker
+from folium.plugins.fast_marker_cluster import FastMarkerCluster
+from folium.plugins.float_image import FloatImage
+from folium.plugins.fullscreen import Fullscreen
+from folium.plugins.heat_map import HeatMap
+from folium.plugins.image_overlay import ImageOverlay
+from folium.plugins.marker_cluster import MarkerCluster
+from folium.plugins.measure_control import MeasureControl
+from folium.plugins.polyline_text_path import PolyLineTextPath
+from folium.plugins.scroll_zoom_toggler import ScrollZoomToggler
+from folium.plugins.terminator import Terminator
+from folium.plugins.timestamped_geo_json import TimestampedGeoJson
+from folium.plugins.timestamped_wmstilelayer import TimestampedWmsTileLayers
 
 __all__ = [
     'BoatMarker',
