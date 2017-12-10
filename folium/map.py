5cf16992b240e8ae50578496990a91d7f5c650de
John Henderson <jw.hendy@gmail.com>
2017-12-10 11:56:46 -0600

    goofed a curly brace

--- folium/map.py
+++ folium/map.py
@@ -289,7 +289,7 @@ class Popup(Element):
 
         self._template = Template(u"""
             var {{this.get_name()}} = L.popup({maxWidth: '{{this.max_width}}'
-            {% if this.show or this.sticky %}, autoClose: false{% endif %}} 
+            {% if this.show or this.sticky %}, autoClose: false{% endif %}
             {% if this.sticky %}, closeOnClick: false{% endif %}});
 
             {% for name, element in this.html._children.items() %}
