80fa0beafd2df5ff968b8c896364e6da22515d4a
Conengmo <33519926+Conengmo@users.noreply.github.com>
2018-06-19 16:39:02 +0200

    Update url in test

--- tests/plugins/test_heat_map_withtime.py
+++ tests/plugins/test_heat_map_withtime.py
@@ -32,7 +32,7 @@ def test_heat_map_with_time():
     # We verify that the script imports are present.
     script = '<script src="https://rawgit.com/socib/Leaflet.TimeDimension/master/dist/leaflet.timedimension.min.js"></script>'  # noqa
     assert script in out
-    script = '<script src="https://cdnjs.cloudflare.com/ajax/libs/heatmap.js/2.0.2/heatmap.min.js"></script>'  # noqa
+    script = '<script src="https://rawgit.com/python-visualization/folium/master/folium/templates/pa7_hm.min.js"></script>'  # noqa
     assert script in out
     script = '<script src="https://rawgit.com/pa7/heatmap.js/develop/plugins/leaflet-heatmap/leaflet-heatmap.js"></script>'  # noqa
     assert script in out
