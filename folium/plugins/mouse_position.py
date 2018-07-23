00081d837c9d862d6dbed96c333b54cdf040159a
btozer <btozer@ucsd.edu>
2018-07-22 23:48:03 -0700

    Update mouse_position.py
    Parameters doc string reformatted. 
    Licence shifted to header. 
    __init__ < 80 chars.

--- folium/plugins/mouse_position.py
+++ folium/plugins/mouse_position.py
@@ -1,5 +1,29 @@
 # -*- coding: utf-8 -*-
 
+# CONVERTED TO PYTHON FROM: https://github.com/ardhi/Leaflet.MousePosition
+# With the MIT License as below.
+
+# Copyright 2012 Ardhi Lukianto
+
+# Permission is hereby granted, free of charge, to any person obtaining
+# a copy of this software and associated documentation files (the
+# "Software"), to deal in the Software without restriction, including
+# without limitation the rights to use, copy, modify, merge, publish,
+# distribute, sublicense, and/or sell copies of the Software, and to
+# permit persons to whom the Software is furnished to do so, subject to
+# the following conditions:
+
+# The above copyright notice and this permission notice shall be
+# included in all copies or substantial portions of the Software.
+
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+# EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+# MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+# NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+# LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+# OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+# WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
 from __future__ import (absolute_import, division, print_function)
 
 import json
@@ -8,43 +32,14 @@ from branca.element import CssLink, Figure, JavascriptLink, MacroElement
 
 from jinja2 import Template
 
-
 class MousePosition(MacroElement):
     """
-    Adds a mouse position widget on the map.
 
     Parameters
     ----------
-    position: location of the widget
-        default is 'bottomright'.
-
-    CONVERTED TO PYTHON FROM:
-
-    https://github.com/ardhi/Leaflet.MousePosition
-
-    With the MIT License as below:
-
-    Copyright 2012 Ardhi Lukianto
-
-    Permission is hereby granted, free of charge, to any person obtaining
-    a copy of this software and associated documentation files (the
-    "Software"), to deal in the Software without restriction, including
-    without limitation the rights to use, copy, modify, merge, publish,
-    distribute, sublicense, and/or sell copies of the Software, and to
-    permit persons to whom the Software is furnished to do so, subject to
-    the following conditions:
-
-    The above copyright notice and this permission notice shall be
-    included in all copies or substantial portions of the Software.
-
-    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
-    EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
-    MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
-    NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
-    LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
-    OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
-    WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-
+    position : str, default 'bottomright'
+        Location of the widget.
+        
     """
     _template = Template("""
         {% macro script(this, kwargs) %}
@@ -55,9 +50,11 @@ class MousePosition(MacroElement):
         {% endmacro %}
         """)  # noqa
 
-    def __init__(self, position='bottomright', separator=' : ', emptyString='Unavailable', lngFirst=False, numDigits=5,
-                 lngFormatter=None, latFormatter=None, prefix=""):
-        """Coordinate, linear, and area measure control"""
+    def __init__(self, position='bottomright', separator=' : ', 
+                 emptyString='Unavailable', lngFirst=False, 
+                 numDigits=5, lngFormatter=None, latFormatter=None, 
+                 prefix=""):
+        
         super(MousePosition, self).__init__()
         self._name = 'MousePosition'
 
