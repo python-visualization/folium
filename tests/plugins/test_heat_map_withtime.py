aa8af6cc349039fdf73835b3a3e4f7ef2c599f8f
Frank <33519926+Conengmo@users.noreply.github.com>
2018-08-01 08:56:55 +0200

    Fix test

--- tests/plugins/test_heat_map_withtime.py
+++ tests/plugins/test_heat_map_withtime.py
@@ -16,6 +16,10 @@ from jinja2 import Template
 import numpy as np
 
 
+def _normalize(rendered):
+    return ''.join(rendered.split())
+
+
 def test_heat_map_with_time():
     np.random.seed(3141592)
     initial_data = (np.random.normal(size=(100, 2)) * np.array([[1, 1]]) +
@@ -41,45 +45,46 @@ def test_heat_map_with_time():
 
     # We verify that the script part is correct.
     tmpl = Template("""
-            var times = {{this.times}};
-
-            {{this._parent.get_name()}}.timeDimension = L.timeDimension(
-                {times : times, currentTime: new Date(1)}
-            );
-
-            var {{this._control_name}} = new L.Control.TimeDimensionCustom({{this.index}}, {
-                autoPlay: {{this.auto_play}},
-                backwardButton: {{this.backward_button}},
-                displayDate: {{this.display_index}},
-                forwardButton: {{this.forward_button}},
-                limitMinimumRange: {{this.limit_minimum_range}},
-                limitSliders: {{this.limit_sliders}},
-                loopButton: {{this.loop_button}},
-                maxSpeed: {{this.max_speed}},
-                minSpeed: {{this.min_speed}},
-                playButton: {{this.play_button}},
-                playReverseButton: {{this.play_reverse_button}},
-                position: "{{this.position}}",
-                speedSlider: {{this.speed_slider}},
-                speedStep: {{this.speed_step}},
-                styleNS: "{{this.style_NS}}",
-                timeSlider: {{this.time_slider}},
-                timeSliderDrapUpdate: {{this.time_slider_drap_update}},
-                timeSteps: {{this.index_steps}}
-                })
-                .addTo({{this._parent.get_name()}});
-
-                var {{this.get_name()}} = new TDHeatmap({{this.data}},
-                {heatmapOptions: {
-                        radius: {{this.radius}},
-                        minOpacity: {{this.min_opacity}},
-                        maxOpacity: {{this.max_opacity}},
-                        scaleRadius: {{this.scale_radius}},
-                        useLocalExtrema: {{this.use_local_extrema}},
-                        defaultWeight: 1 ,
-                    }
-                })
-                .addTo({{this._parent.get_name()}});
+        var times = {{this.times}};
+    
+        {{this._parent.get_name()}}.timeDimension = L.timeDimension(
+            {times : times, currentTime: new Date(1)}
+        );
+    
+        var {{this._control_name}} = new L.Control.TimeDimensionCustom({{this.index}}, {
+            autoPlay: {{this.auto_play}},
+            backwardButton: {{this.backward_button}},
+            displayDate: {{this.display_index}},
+            forwardButton: {{this.forward_button}},
+            limitMinimumRange: {{this.limit_minimum_range}},
+            limitSliders: {{this.limit_sliders}},
+            loopButton: {{this.loop_button}},
+            maxSpeed: {{this.max_speed}},
+            minSpeed: {{this.min_speed}},
+            playButton: {{this.play_button}},
+            playReverseButton: {{this.play_reverse_button}},
+            position: "{{this.position}}",
+            speedSlider: {{this.speed_slider}},
+            speedStep: {{this.speed_step}},
+            styleNS: "{{this.style_NS}}",
+            timeSlider: {{this.time_slider}},
+            timeSliderDrapUpdate: {{this.time_slider_drap_update}},
+            timeSteps: {{this.index_steps}}
+            })
+            .addTo({{this._parent.get_name()}});
+    
+            var {{this.get_name()}} = new TDHeatmap({{this.data}},
+            {heatmapOptions: {
+                    radius: {{this.radius}},
+                    minOpacity: {{this.min_opacity}},
+                    maxOpacity: {{this.max_opacity}},
+                    scaleRadius: {{this.scale_radius}},
+                    useLocalExtrema: {{this.use_local_extrema}},
+                    defaultWeight: 1,
+                    {% if this.gradient %}gradient: {{ this.gradient }}{% endif %}
+                }
+            })
+            .addTo({{this._parent.get_name()}});
     """)
 
-    assert tmpl.render(this=hm)
+    assert _normalize(tmpl.render(this=hm)) in _normalize(out)
