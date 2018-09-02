5c27d3190aaab916756a453fc734f56736088e9a
Frank <33519926+Conengmo@users.noreply.github.com>
2018-09-02 22:23:03 +0200

    Move attribution to docstring

--- folium/plugins/mouse_position.py
+++ folium/plugins/mouse_position.py
@@ -1,29 +1,5 @@
 # -*- coding: utf-8 -*-
 
-# CONVERTED TO PYTHON FROM: https://github.com/ardhi/Leaflet.MousePosition
-# With the MIT License as below.
-
-# Copyright 2012 Ardhi Lukianto
-
-# Permission is hereby granted, free of charge, to any person obtaining
-# a copy of this software and associated documentation files (the
-# "Software"), to deal in the Software without restriction, including
-# without limitation the rights to use, copy, modify, merge, publish,
-# distribute, sublicense, and/or sell copies of the Software, and to
-# permit persons to whom the Software is furnished to do so, subject to
-# the following conditions:
-
-# The above copyright notice and this permission notice shall be
-# included in all copies or substantial portions of the Software.
-
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
-# EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
-# MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
-# NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
-# LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
-# OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
-# WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-
 from __future__ import (absolute_import, division, print_function)
 
 import json
@@ -34,7 +10,10 @@ from jinja2 import Template
 
 
 class MousePosition(MacroElement):
-    """
+    """Add a field that shows the coordinates of the mouse position.
+
+    Uses the Leaflet plugin by Ardhi Lukianto under MIT license.
+    https://github.com/ardhi/Leaflet.MousePosition
 
     Parameters
     ----------
