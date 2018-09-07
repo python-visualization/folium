6e87103d0098c8515ddc2a74785c18cf02c93028
Demetrio Rodriguez <Demetrio.Rodriguez.T@gmail.com>
2018-09-07 18:23:20 +0200

    move example into the function docstring https://github.com/python-visualization/folium/pull/953#discussion_r215041660

--- folium/folium.py
+++ folium/folium.py
@@ -400,6 +400,23 @@ $(document).ready(objects_in_front);
         port: int
             Corresponding port, default 7000
 
+        Examples
+        --------
+        import numpy as np
+        import folium
+        from folium.plugins import HeatMap
+
+        # create a dummy map
+        data = (np.random.normal(size=(100, 3)) *
+                np.array([[1, 1, 1]]) +
+                np.array([[48, 5, 1]])).tolist()
+        folium_map = folium.Map([48., 5.], tiles='stamentoner', zoom_start=6)
+        HeatMap(data).add_to(folium_map)
+
+        # open in in browser
+        folium_map.show_in_browser()
+
+
         """
         srvr = TemporaryHTTPServer(host, port)
         srvr.open_html_in_browser(self._repr_html_())
