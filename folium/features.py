071b22073a0627108cb73e2c7dc848d748a003b7
jtbaker <jason.th.baker@gmail.com>
2018-10-16 07:23:32 -0500

    Rework check to class method.
    Fix last string quote from triple to single.

--- folium/features.py
+++ folium/features.py
@@ -705,19 +705,23 @@ class GeoJsonTooltip(Tooltip):
             # noqa outside of type checking.
             self.style = style
 
+    def warn_for_geometry_collections(self):
+        """Checks for GeoJson GeometryCollection features to warn user about incompatibility."""
+        geom_collections = [
+            feature['id'] for feature in self._parent.data['features']
+            if feature['geometry']['type'] == 'GeometryCollection'
+        ]
+        if any(geom_collections):
+            warnings.warn(
+                "GeoJsonTooltip is not configured to render tooltips for GeoJson GeometryCollection geometries. "
+                "Please consider reworking feature IDs {} to MultiPolygon for full functionality.\n"
+                "https://tools.ietf.org/html/rfc7946#page-9".format(geom_collections), UserWarning)
+
     def render(self, **kwargs):
         """Renders the HTML representation of the element."""
         if isinstance(self._parent, GeoJson):
             keys = tuple(self._parent.data['features'][0]['properties'].keys())
-            geom_collections = [
-                feature['id'] for feature in self._parent.data['features']
-                if feature['geometry']['type'] == 'GeometryCollection'
-            ]
-            if any(geom_collections):
-                warnings.warn(
-                    "GeoJsonTooltip is not configured to render tooltips for GeoJson GeometryCollection geometries. "
-                    "Please consider reworking feature IDs {} to MultiPolygon for full functionality.\n"
-                    "https://tools.ietf.org/html/rfc7946#page-9""".format(geom_collections), UserWarning)
+            self.warn_for_geometry_collections()
         elif isinstance(self._parent, TopoJson):
             obj_name = self._parent.object_path.split('.')[-1]
             keys = tuple(self._parent.data['objects'][obj_name][
