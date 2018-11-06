4c5b37c0c7b969e02b6c35e4d4a8e7c113aa1e03
cclauss <cclauss@bluewin.ch>
2018-11-06 23:55:17 +0100

    Add Python 3.7 to the PyPI trove classifiers in setup.py
    These must have been some glitch in the last release to PyPI because these trove classifiers should appear in the leftmost column in https://pypi.org/project/folium/ instead at the very bottom of the README.md txt.

--- setup.py
+++ setup.py
@@ -65,6 +65,7 @@ setup(
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
         'Topic :: Scientific/Engineering :: GIS',
         'Topic :: Scientific/Engineering :: Visualization',
         'License :: OSI Approved :: MIT License',
