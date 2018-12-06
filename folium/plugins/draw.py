112cdc0bcb82a424118dba2d372891f059e20be2
Dmitry Nikolaev <etsu.nikolaev@gmail.com>
2018-12-06 16:41:10 +0300

    add options to draw control

--- folium/plugins/draw.py
+++ folium/plugins/draw.py
@@ -1,6 +1,9 @@
 # -*- coding: utf-8 -*-
 
+
 from __future__ import (absolute_import, division, print_function)
+
+import json
 
 from branca.element import CssLink, Element, Figure, JavascriptLink, MacroElement
 
@@ -10,28 +13,36 @@ from jinja2 import Template
 class Draw(MacroElement):
     """
     Vector drawing and editing plugin for Leaflet.
-
     Parameters
     ----------
     export : bool, default False
         Add a small button that exports the drawn shapes as a geojson file.
-
+    filename : string, default data.geojson
+        Name of geojson file
+    position : string, default 'topleft'
+        Position of control. It can be one of 'topleft', 'toprigth', 'bottomleft', 'bottomright'
+    draw : dict, default None
+        The options used to configure the draw toolbar
+    edit : dict, default None
+        The options used to configure the edit toolbar
     Examples
     --------
     >>> m = folium.Map()
     >>> Draw(export=True).add_to(m)
-
     For more info please check
     https://leaflet.github.io/Leaflet.draw/docs/leaflet-draw-latest.html
-
     """
     _template = Template(u"""
             {% macro script(this, kwargs) %}
+            var options = {
+              position: "{{kwargs.get('position', 'topleft')}}",
+              draw: {{kwargs.get('draw', {})}},
+              edit: {{kwargs.get('edit', {})}}
+            }
             // FeatureGroup is to store editable layers.
             var drawnItems = new L.featureGroup().addTo({{this._parent.get_name()}});
-            var {{this.get_name()}} = new L.Control.Draw({
-                "edit": {"featureGroup": drawnItems}
-                }).addTo({{this._parent.get_name()}})
+            options.edit.featureGroup = drawnItems
+            var {{this.get_name()}} = new L.Control.Draw(options).addTo({{this._parent.get_name()}})
             {{this._parent.get_name()}}.on(L.Draw.Event.CREATED, function (event) {
               var layer = event.layer,
                   type = event.layerType,
@@ -43,37 +54,48 @@ class Draw(MacroElement):
                 });
                drawnItems.addLayer(layer);
              });
-
-        {{this._parent.get_name()}}.on('draw:created', function(e) {
-            drawnItems.addLayer(e.layer);
-        });
-
-        document.getElementById('export').onclick = function(e) {
-            var data = drawnItems.toGeoJSON();
-            var convertedData = 'text/json;charset=utf-8,' + encodeURIComponent(JSON.stringify(data));
-            document.getElementById('export').setAttribute('href', 'data:' + convertedData);
-            document.getElementById('export').setAttribute('download','data.geojson');
-        }
+            {{this._parent.get_name()}}.on('draw:created', function(e) {
+                drawnItems.addLayer(e.layer);
+            });
+            document.getElementById('export').onclick = function(e) {
+              var data = drawnItems.toGeoJSON();
+              var convertedData = 'text/json;charset=utf-8,' + encodeURIComponent(JSON.stringify(data));
+              document.getElementById('export').setAttribute('href', 'data:' + convertedData);
+              document.getElementById('export').setAttribute(
+                'download',
+                "{{kwargs.get('filename', 'data.geojson')}}"
+              );
+            }
             {% endmacro %}
             """)
 
-    def __init__(self, export=False):
+    js_url = 'https://cdnjs.cloudflare.com/ajax/libs/leaflet.draw/1.0.2/leaflet.draw.js'
+    css_url = 'https://cdnjs.cloudflare.com/ajax/libs/leaflet.draw/1.0.2/leaflet.draw.css'
+
+    def __init__(self, export=False, filename='data.geojson',
+                 position='topleft', draw=None, edit=None):
         super(Draw, self).__init__()
         self._name = 'DrawControl'
         self.export = export
+        self.filename = filename
+        self.position = position
+        self.draw = draw or {}
+        self.edit = edit or {}
 
     def render(self, **kwargs):
-        super(Draw, self).render()
+        super(Draw, self).render(
+            position=self.position,
+            draw=json.dumps(self.draw),
+            edit=json.dumps(self.edit),
+            filename=self.filename
+        )
 
         figure = self.get_root()
         assert isinstance(figure, Figure), ('You cannot render this Element '
                                             'if it is not in a Figure.')
 
-        figure.header.add_child(
-            JavascriptLink('https://cdnjs.cloudflare.com/ajax/libs/leaflet.draw/1.0.2/leaflet.draw.js'))  # noqa
-
-        figure.header.add_child(
-            CssLink('https://cdnjs.cloudflare.com/ajax/libs/leaflet.draw/1.0.2/leaflet.draw.css'))  # noqa
+        figure.header.add_child(JavascriptLink(self.js_url))
+        figure.header.add_child(CssLink(self.css_url))
 
         export_style = """<style>
         #export {
