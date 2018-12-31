ea6daace9bcf3daaf203e54e0d638cfbcf279c6a
Frank <33519926+Conengmo@users.noreply.github.com>
2018-12-31 09:19:22 +0100

    Add test for get_obj_in_upper_tree

--- tests/test_utilities.py
+++ tests/test_utilities.py
@@ -1,6 +1,9 @@
 from __future__ import (absolute_import, division, print_function)
 
-from folium.utilities import camelize
+import pytest
+
+from folium.utilities import camelize, get_obj_in_upper_tree
+from folium import Map, FeatureGroup, Marker, Popup
 
 
 def test_camelize():
@@ -8,3 +11,16 @@ def test_camelize():
     assert camelize('variableName') == 'variableName'
     assert camelize('name') == 'name'
     assert camelize('very_long_variable_name') == 'veryLongVariableName'
+
+
+def test_get_obj_in_upper_tree():
+    m = Map()
+    fg = FeatureGroup().add_to(m)
+    marker = Marker(location=(0, 0)).add_to(fg)
+    assert get_obj_in_upper_tree(marker, FeatureGroup) is fg
+    assert get_obj_in_upper_tree(marker, Map) is m
+    # The search should only go up, not down:
+    with pytest.raises(ValueError):
+        assert get_obj_in_upper_tree(fg, Marker)
+    with pytest.raises(ValueError):
+        assert get_obj_in_upper_tree(marker, Popup)
