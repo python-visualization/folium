1036708d971a8ffcd0d220dc03ef5af08a040c73
Dustin Mendoza <penguindustin@gmail.com>
2019-05-04 14:02:46 -0700

    fixed ci issues

--- examples/flask_example.py
+++ examples/flask_example.py
@@ -1,7 +1,8 @@
-from flask import Flask, render_template
+from flask import Flask
 import folium
 
 app = Flask(__name__)
+
 
 @app.route('/')
 def index():
@@ -9,5 +10,7 @@ def index():
     folium_map = folium.Map(location=start_coords, zoom_start=14)
     return folium_map._repr_html_()
 
+
 if __name__ == '__main__':
     app.run(debug=True)
+    
