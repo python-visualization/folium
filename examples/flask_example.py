12d183802ef336739633d983941d7a0ce7ef2fd2
Dustin Mendoza <penguindustin@gmail.com>
2019-05-08 19:28:29 -0700

    added example usage

--- examples/flask_example.py
+++ examples/flask_example.py
@@ -1,3 +1,18 @@
+""" flask_example.py
+
+    Required packages:
+    - flask
+    - folium
+    
+    Usage:
+    
+    Start the flask server by running:
+
+        $ python flask_example.py
+    
+    And then head to http://127.0.0.1:5000/ in your browser to see the map displayed
+
+"""
 from flask import Flask
 import folium
 
