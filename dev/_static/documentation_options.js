6f15e2aa727dfb36f871359198c2a6b440744fd5
Doctr (Travis CI) <drdoctr@users.noreply.github.com>
2019-05-17 17:05:17 +0000

    Update docs after building Travis build 2086 of python-visualization/folium
    The docs were built from the branch 'master' against the commit
    f6da1e727f9379b0ae111c88db0368e8483146bf.
    
    The Travis build that generated this commit is at
    https://travis-ci.org/python-visualization/folium/jobs/533901143.
    
    The doctr command that was run is
    
        /home/travis/miniconda/envs/TEST/lib/python3.7/site-packages/doctr/__main__.py deploy --build-tags --key-path github_deploy_key_python_visualization_folium.enc --built-docs docs/_build/html/ dev

--- dev/_static/documentation_options.js
+++ dev/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.8.3+52.g2758dc7',
+    VERSION: '0.8.3+54.gf6da1e7',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
