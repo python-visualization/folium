a88d68fca54ef0324be5ab3d84e8f0e8891beae9
Filipe Fernandes <ocefpaf@gmail.com>
2019-05-26 15:40:46 -0300

    fix lints

--- examples/flask_example.py
+++ examples/flask_example.py
@@ -3,17 +3,19 @@
     Required packages:
     - flask
     - folium
-    
+
     Usage:
-    
+
     Start the flask server by running:
 
         $ python flask_example.py
-    
+
     And then head to http://127.0.0.1:5000/ in your browser to see the map displayed
 
 """
+
 from flask import Flask
+
 import folium
 
 app = Flask(__name__)
