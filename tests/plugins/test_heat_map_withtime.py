394298dbbfccb61e7b7b97c99f1ff7f28aaa72ba
Massimo Santini <massimo.santini@gmail.com>
2019-11-14 10:30:22 +0100

    This should fix the test

--- tests/plugins/test_heat_map_withtime.py
+++ tests/plugins/test_heat_map_withtime.py
@@ -26,13 +26,13 @@ def test_heat_map_with_time():
     out = normalize(m._parent.render())
 
     # We verify that the script imports are present.
-    script = '<script src="https://rawcdn.githack.com/socib/Leaflet.TimeDimension/master/dist/leaflet.timedimension.min.js"></script>'  # noqa
+    script = '<script src="https://cdn.jsdelivr.net/npm/leaflet-timedimension@1.1.0/dist/leaflet.timedimension.min.js"></script>'  # noqa
     assert script in out
     script = '<script src="https://rawcdn.githack.com/python-visualization/folium/master/folium/templates/pa7_hm.min.js"></script>'  # noqa
     assert script in out
     script = '<script src="https://rawcdn.githack.com/pa7/heatmap.js/develop/plugins/leaflet-heatmap/leaflet-heatmap.js"></script>'  # noqa
     assert script in out
-    script = '<link rel="stylesheet" href="http://apps.socib.es/Leaflet.TimeDimension/dist/leaflet.timedimension.control.min.css"/>'  # noqa
+    script = '<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/leaflet-timedimension@1.1.1/dist/leaflet.timedimension.control.min.css"/>'  # noqa
     assert script in out
 
     # We verify that the script part is correct.
