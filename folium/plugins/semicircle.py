7c81009e9f52c54f46a187cae04296e6dadd9c40
kuaka <stuart.mackintosh@gmail.com>
2019-12-19 16:06:28 +1300

    addresses requested changes for original (Add SemiCircle Plugin #1050) pull request into folium. Also adds color options from https://stackoverflow.com/questions/57227249/folium-how-can-i-change-fill-color-in-semicircle-plugin

--- folium/plugins/semicircle.py
+++ folium/plugins/semicircle.py
@@ -16,48 +16,85 @@ class SemiCircle(Marker):
     """
     Creates a Semicircle plugin to append into a map with
     Map.add_plugin.
-    Use (direction and arc) or (startAngle and stopAngle)
+    Use (direction and arc) or (start_angle and stop_angle)
     Parameters
     ----------
     location: tuple of length 2, default None
         The latitude and longitude of the marker.
         If None, then the middle of the map is used.
     radius: int, default 0
-        Radius of semicircle
+        Radius of semicircle in meters
     direction: int, default 0
         Heading of direction angle value between 0 and 360 degrees
     arc: int, default 0
         Heading of arc angle value between 0 and 360 degrees.
-    startAngle: int, default 0
+    start_angle: int, default 0
         Heading of the start angle value between 0 and 360 degrees
-    stopAngle: int, default 0
+    stop_angle: int, default 0
         Heading of the stop angle value between 0 and 360 degrees.
+    fill_color: string, default '#3388ff' (blue)
+        Fill color
+    fill_opacity: float 0-1, default 0.5
+        Fill opacity
+    color: string, default '#3388ff' (blue)
+        Line color
+    opacity: float 0-1, default 1
+        Line opacity
+    **kwargs:
+        Passed to Marker so a popup and tooltip can be provided.
+
     """
     _template = Template(u"""
             {% macro script(this, kwargs) %}
                 if ({{this.direction}} || {{this.arc}}) {
                     var {{this.get_name()}} = L.semiCircle(
                         [{{this.location[0]}},{{this.location[1]}}],
-                        {radius:{{this.radius}}}).setDirection({{this.direction}},{{this.arc}})
+                        {radius:{{this.radius}},
+                        fill: {{this.fill}},
+                        fillColor:'{{this.fill_color}}',
+                        fillOpacity: {{this.fill_opacity}},
+                        color: '{{this.color}}',
+                        opacity: {{this.opacity}}
+                        }).setDirection({{this.direction}},{{this.arc}})
                         .addTo({{this._parent.get_name()}});
-                } else if ({{this.startAngle}} || {{this.stopAngle}}) {
+                } else if ({{this.start_angle}} || {{this.stop_angle}}) {
                     var {{this.get_name()}} = L.semiCircle(
                         [{{this.location[0]}},{{this.location[1]}}],
-                        {radius:{{this.radius}}, startAngle:{{this.startAngle}}, stopAngle:{{this.stopAngle}}})
+                        {radius:{{this.radius}},
+                        fill: {{this.fill}},
+                        fillColor:'{{this.fill_color}}',
+                        fillOpacity: {{this.fill_opacity}},
+                        color: '{{this.color}}',
+                        opacity: {{this.opacity}},
+                        startAngle:{{this.start_angle}}, 
+                        stopAngle:{{this.stop_angle}}
+                        })
                         .addTo({{this._parent.get_name()}});
                 }
             {% endmacro %}
             """)
 
-    def __init__(self, location, radius=0, direction=0, arc=0, startAngle=0, stopAngle=0, **kwargs):
-        super(SemiCircle, self).__init__( _validate_location(location), **kwargs)
+    def __init__(self, location, radius=0, direction=None, arc=None, start_angle=None, stop_angle=None, fill=True,
+                 fill_color='#3388ff', fill_opacity=0.5, color='#3388ff', opacity=1, **kwargs):
+        super(SemiCircle, self).__init__(_validate_location(location), **kwargs)
         self._name = 'SemiCircle'
         self.radius = radius
-        self.direction = direction
-        self.arc = arc
-        self.startAngle = startAngle
-        self.stopAngle = stopAngle
+        self.direction = json.dumps(direction)
+        self.arc = json.dumps(arc)
+        self.start_angle = json.dumps(start_angle)
+        self.stop_angle = json.dumps(stop_angle)
+        if fill:
+            self.fill = 'true'
+        else:
+            self.fill = 'false'
+        self.fill_color = fill_color
+        self.fill_opacity = fill_opacity
+        self.color = color
+        self.opacity = opacity
         self.kwargs = json.dumps(kwargs)
+
+        if not (direction and arc) and not (start_angle and stop_angle):
+            raise ValueError("Invalid arguments. Either provide direction and arc or start_angle and stop_angle")
 
     def render(self, **kwargs):
         super(SemiCircle, self).render(**kwargs)
@@ -67,5 +104,5 @@ class SemiCircle(Marker):
                                             'if it is not in a Figure.')
 
         figure.header.add_child(
-            JavascriptLink('http://jieter.github.io/Leaflet-semicircle/Semicircle.js'),
-name='semicirclejs')
+            JavascriptLink('https://cdn.jsdelivr.net/npm/leaflet-semicircle@2.0.2/Semicircle.min.js'),
+            name='semicirclejs')
