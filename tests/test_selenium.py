d8b1c10e7935d220017c9fdff9f243734f474901
Frank <33519926+Conengmo@users.noreply.github.com>
2020-04-26 15:06:16 +0200

    Create test_selenium.py

---
+++ tests/test_selenium.py
@@ -0,0 +1,48 @@
+import glob
+import os
+import subprocess
+from contextlib import contextmanager
+
+import nbconvert
+from parameterized import parameterized
+from seleniumbase import BaseCase
+
+
+class TestNotebooks(BaseCase):
+
+    @parameterized.expand(glob.glob('examples/*.ipynb'))
+    def test_notebook(self, filepath):
+        with get_notebook_html(filepath) as filepath_html:
+            self.open('file://' + filepath_html)
+            self.assert_element('iframe')
+            iframes = self.find_elements('iframe')
+            for iframe in iframes:
+                self.switch_to_frame(iframe)
+                self.assert_element('.folium-map')
+                self.switch_to_default_content()
+            # logs don't work in firefox, use chrome
+            logs = self.driver.get_log("browser")
+            for log in logs:
+                if log['level'] == 'SEVERE':
+                    msg = ' '.join(log['message'].split()[2:])
+                    raise RuntimeError('Javascript error: "{}".'.format(msg))
+
+
+@contextmanager
+def get_notebook_html(filepath_notebook, run=True):
+    if run:
+        subprocess.run(['jupyter', 'nbconvert', '--to', 'notebook', '--execute',
+                        '--inplace', filepath_notebook])
+    html_exporter = nbconvert.HTMLExporter()
+    html_exporter.template_file = 'basic'
+    body, _ = html_exporter.from_filename(filepath_notebook)
+    html = ('<!DOCTYPE html><html><head><meta charset="UTF-8"></head>'
+            '<body>{}</body></html>').format(body)
+    filepath_html = filepath_notebook.replace('.ipynb', '.html')
+    filepath_html = os.path.abspath(filepath_html)
+    with open(filepath_html, 'w') as f:
+        f.write(html)
+    try:
+        yield filepath_html
+    finally:
+        os.remove(filepath_html)
