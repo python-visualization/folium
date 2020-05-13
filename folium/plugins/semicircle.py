ad7ed87cdcb296e4bf192cc3e4e7348c05aeb4ea
kuaka <stuart.mackintosh@gmail.com>
2020-05-13 22:17:13 +1200

    incorporates Conengmo's suggestions/requests from review. also adds unit test for semicircle plugin.

--- folium/plugins/semicircle.py
+++ folium/plugins/semicircle.py
@@ -1,13 +1,9 @@
 # -*- coding: utf-8 -*-
 
-from __future__ import (absolute_import, division, print_function)
-
-import json
-
 from branca.element import Figure, JavascriptLink
 
 from folium.map import Marker
-from folium.utilities import _validate_location
+from folium.utilities import parse_options
 
 from jinja2 import Template
 
@@ -41,56 +37,37 @@ class SemiCircle(Marker):
         Line opacity
     **kwargs:
         Passed to Marker so a popup and tooltip can be provided.
-
+    https://github.com/jieter/Leaflet-semicircle
     """
     _template = Template(u"""
             {% macro script(this, kwargs) %}
-                if ({{this.direction}} || {{this.arc}}) {
-                    var {{this.get_name()}} = L.semiCircle(
-                        [{{this.location[0]}},{{this.location[1]}}],
-                        {radius:{{this.radius}},
-                        fill: {{this.fill}},
-                        fillColor:'{{this.fill_color}}',
-                        fillOpacity: {{this.fill_opacity}},
-                        color: '{{this.color}}',
-                        opacity: {{this.opacity}}
-                        }).setDirection({{this.direction}},{{this.arc}})
-                        .addTo({{this._parent.get_name()}});
-                } else if ({{this.start_angle}} || {{this.stop_angle}}) {
-                    var {{this.get_name()}} = L.semiCircle(
-                        [{{this.location[0]}},{{this.location[1]}}],
-                        {radius:{{this.radius}},
-                        fill: {{this.fill}},
-                        fillColor:'{{this.fill_color}}',
-                        fillOpacity: {{this.fill_opacity}},
-                        color: '{{this.color}}',
-                        opacity: {{this.opacity}},
-                        startAngle:{{this.start_angle}},
-                        stopAngle:{{this.stop_angle}}
-                        })
-                        .addTo({{this._parent.get_name()}});
-                }
+                    var {{ this.get_name() }} = L.semiCircle(
+                        [{{ this.location[0] }},{{ this.location[1] }}],
+                        {{ this.options | tojson }}
+                        )
+                        {% if this.direction %}
+                            .setDirection{{ this.direction }}
+                        {% endif %}
+                        .addTo({{ this._parent.get_name() }});
             {% endmacro %}
             """)
 
     def __init__(self, location, radius, direction=None, arc=None, start_angle=None, stop_angle=None, fill=True,
                  fill_color='#3388ff', fill_opacity=0.5, color='#3388ff', opacity=1, **kwargs):
-        super(SemiCircle, self).__init__(_validate_location(location), **kwargs)
+        super(SemiCircle, self).__init__(location, **kwargs)
         self._name = 'SemiCircle'
-        self.radius = radius
-        self.direction = json.dumps(direction)
-        self.arc = json.dumps(arc)
-        self.start_angle = json.dumps(start_angle)
-        self.stop_angle = json.dumps(stop_angle)
-        if fill:
-            self.fill = 'true'
-        else:
-            self.fill = 'false'
-        self.fill_color = fill_color
-        self.fill_opacity = fill_opacity
-        self.color = color
-        self.opacity = opacity
-        self.kwargs = json.dumps(kwargs)
+        self.direction = (direction, arc) if direction and arc else None
+        self.options = parse_options(
+            radius=radius,
+            start_angle=start_angle,
+            stop_angle=stop_angle,
+            fill_color=fill_color,
+            fill_opacity=fill_opacity,
+            color=color,
+            opacity=opacity,
+            fill='true' if fill else 'false',
+            **kwargs
+        )
 
         if not ((direction is None and arc is None) and (start_angle is not None and stop_angle is not None)
                 or (direction is not None and arc is not None) and (start_angle is None and stop_angle is None)):
@@ -104,5 +81,5 @@ class SemiCircle(Marker):
                                             'if it is not in a Figure.')
 
         figure.header.add_child(
-            JavascriptLink('https://cdn.jsdelivr.net/npm/leaflet-semicircle@2.0.2/Semicircle.min.js'),
+            JavascriptLink('https://cdn.jsdelivr.net/npm/leaflet-semicircle@2.0.4/Semicircle.min.js'),
             name='semicirclejs')
