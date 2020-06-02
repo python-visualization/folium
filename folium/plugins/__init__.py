8954f28e1bc3d45fb62ccd46a988ea3ef4e0654f
Thiru <thiru1337@gmail.com>
2020-06-02 21:41:32 +0800

    Add Test

--- folium/plugins/__init__.py
+++ folium/plugins/__init__.py
@@ -34,8 +34,8 @@ from folium.plugins.semicircle import SemiCircle
 from folium.plugins.terminator import Terminator
 from folium.plugins.time_slider_choropleth import TimeSliderChoropleth
 from folium.plugins.timestamped_geo_json import TimestampedGeoJson
-from folium.plugins.timestamped_wmstilelayer import TimestampedWmsTileLayers
 from folium.plugins.tag_filter_button import TagFilterButton
+from folium.plugins.timestamped_wmstilelayer import TimestampedWmsTileLayers
 
 __all__ = [
     'AntPath',
