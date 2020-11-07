867fc72e096230854ccc7ccd9bbadcec2fda636c
Frank <33519926+Conengmo@users.noreply.github.com>
2020-11-07 11:29:08 +0100

    Test for Python 3.9

--- setup.py
+++ setup.py
@@ -80,6 +80,8 @@ setup(
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
         'Topic :: Scientific/Engineering :: GIS',
         'Topic :: Scientific/Engineering :: Visualization',
         'License :: OSI Approved :: MIT License',
