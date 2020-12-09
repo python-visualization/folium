baaf05ac90695417376411064e6093421095b9be
k.clasen <k.clasen@campus.tu-berlin.de>
2020-12-09 21:02:17 +0100

    Update GeoJson doc-string

--- folium/features.py
+++ folium/features.py
@@ -324,6 +324,9 @@ class GeoJson(Layer):
         * If dict, then data will be converted to JSON and embedded
         in the JavaScript.
         * If str, then data will be passed to the JavaScript as-is.
+        * If `__geo_interface__` is available, the `__geo_interface__` 
+        dictionary will be serialized to JSON and 
+        reprojected if `to_crs` is available.
     style_function: function, default None
         Function mapping a GeoJson Feature to a style dict.
     highlight_function: function, default None
