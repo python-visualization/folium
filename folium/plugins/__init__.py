b770a391ade20235cf43c8b9575848370ac6ebe3
Benjamin Ramser <legionaerr@googlemail.com>
2022-02-27 09:58:04 +0100

    feat: add Leaflet.VectorGrid plugin

--- folium/plugins/__init__.py
+++ folium/plugins/__init__.py
@@ -33,6 +33,7 @@ from folium.plugins.terminator import Terminator
 from folium.plugins.time_slider_choropleth import TimeSliderChoropleth
 from folium.plugins.timestamped_geo_json import TimestampedGeoJson
 from folium.plugins.timestamped_wmstilelayer import TimestampedWmsTileLayers
+from folium.plugins.vectorgrid_protobuf import VectorGridProtobuf
 
 __all__ = [
     'AntPath',
@@ -63,4 +64,5 @@ __all__ = [
     'TimeSliderChoropleth',
     'TimestampedGeoJson',
     'TimestampedWmsTileLayers',
+    'VectorGridProtobuf'
 ]
