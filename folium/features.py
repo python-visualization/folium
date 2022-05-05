33bbe9ee84205d1fb08773f944ae567a74ee2154
Martin Journois <BibMartin@users.noreply.github.com>
2022-05-05 12:10:24 +0200

    Click to copy latlng to clipboard (#1530)
    * MJ: Add ClickForLatLng class to save LatLng to clipboard
    
    * MJ: Add example notebook to show MouseClick events (ClickForMarker etc)
    
    * MJ: Fix typo clipboads -> clipboard
    
    * Try to fix notebook tests
    
    * use newest branca
    
    Co-authored-by: BibMartin <{ID}+{username}@users.noreply.github.com>
    Co-authored-by: Frank <33519926+Conengmo@users.noreply.github.com>

--- folium/features.py
+++ folium/features.py
@@ -1417,6 +1417,41 @@ class ClickForMarker(MacroElement):
             self.popup = '"Latitude: " + lat + "<br>Longitude: " + lng '
 
 
+class ClickForLatLng(MacroElement):
+    """
+    When one clicks on a Map that contains a ClickForLatLng,
+    the coordinates of the pointer's position are copied to clipboard.
+
+    Parameters
+    ==========
+    format_str : str, default 'lat + "," + lng'
+        The javascript string used to format the text copied to clipboard.
+        eg:
+            format_str = 'lat + "," + lng'              >> 46.558860,3.397397
+            format_str = '"[" + lat + "," + lng + "]"'  >> [46.558860,3.397397]
+    alert : bool, default True
+        Whether there should be an alert when something has been copied to clipboard.
+    """
+    _template = Template(u"""
+            {% macro script(this, kwargs) %}
+                function getLatLng(e){
+                    var lat = e.latlng.lat.toFixed(6),
+                        lng = e.latlng.lng.toFixed(6);
+                    var txt = {{this.format_str}};
+                    navigator.clipboard.writeText(txt);
+                    {% if this.alert %}alert("Copied to clipboard : \\n    " + txt);{% endif %}
+                    };
+                {{this._parent.get_name()}}.on('click', getLatLng);
+            {% endmacro %}
+            """)  # noqa
+
+    def __init__(self, format_str=None, alert=True):
+        super(ClickForLatLng, self).__init__()
+        self._name = 'ClickForLatLng'
+        self.format_str = format_str or 'lat + "," + lng'
+        self.alert = alert
+
+
 class CustomIcon(Icon):
     """
     Create a custom icon, based on an image.
