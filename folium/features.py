3cc93cf71dd006ccc532cbfd085ef11f5a470020
Frank <33519926+Conengmo@users.noreply.github.com>
2022-11-17 14:45:38 +0100

    address comments

--- folium/features.py
+++ folium/features.py
@@ -1236,8 +1236,11 @@ class Choropleth(FeatureGroup):
                 from jenkspy import jenks_breaks
 
                 if not isinstance(bins, int):
-                    raise ValueError(f'bins value must be an integer. Invalid value "{bins}" received.')
-                bin_edges = np.array(jenks_breaks(real_values, bins))
+                    raise ValueError(
+                        f'bins value must be an integer when using Jenks.'
+                        f' Invalid value "{bins}" received.'
+                    )
+                bin_edges = np.array(jenks_breaks(real_values, bins), dtype=float)
             else:
                 _, bin_edges = np.histogram(real_values, bins=bins)
 
