78f3c192d496fc1b411b79580849838a24157ec2
Frank <33519926+Conengmo@users.noreply.github.com>
2022-11-28 13:19:00 +0100

    FloatImage: use any kwargs as CSS

--- folium/plugins/float_image.py
+++ folium/plugins/float_image.py
@@ -3,17 +3,34 @@ from jinja2 import Template
 
 
 class FloatImage(MacroElement):
-    """Adds a floating image in HTML canvas on top of the map."""
+    """Adds a floating image in HTML canvas on top of the map.
+
+    Parameters
+    ----------
+    image: str
+        Url to image location. Can also be an inline image using a data URI
+        or a local file using `file://`.
+    bottom: int, default 75
+        Vertical position from the bottom, as a percentage of screen height.
+    left: int, default 75
+        Horizontal position from the left, as a percentage of screen width.
+    **kwargs
+        Additional keyword arguments are applied as CSS properties.
+        For example: `width='300px'`.
+
+    """
 
     _template = Template(
         """
             {% macro header(this,kwargs) %}
                 <style>
                     #{{this.get_name()}} {
-                        position:absolute;
-                        bottom:{{this.bottom}}%;
-                        left:{{this.left}}%;
-                        width:{{this.width}}%;
+                        position: absolute;
+                        bottom: {{this.bottom}}%;
+                        left: {{this.left}}%;
+                        {%- for property, value in this.css.items() %}
+                          {{ property }}: {{ value }};
+                        {%- endfor %}
                         }
                 </style>
             {% endmacro %}
@@ -27,10 +44,10 @@ class FloatImage(MacroElement):
             """
     )
 
-    def __init__(self, image, bottom=75, left=75, width=100):
+    def __init__(self, image, bottom=75, left=75, **kwargs):
         super().__init__()
         self._name = "FloatImage"
         self.image = image
         self.bottom = bottom
         self.left = left
-        self.width = width
+        self.css = kwargs
