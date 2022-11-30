d004cb458b4309bdc0d97398d9a13719d259371e
Frank <33519926+Conengmo@users.noreply.github.com>
2022-11-30 13:56:11 +0100

    More flask documentation (#1675)
    * More Flask documentation
    
    * don't use iframe in simple example
    
    * put examples in script

--- examples/flask_example.py
+++ examples/flask_example.py
@@ -14,7 +14,7 @@
 
 """
 
-from flask import Flask
+from flask import Flask, render_template_string
 
 import folium
 
@@ -22,10 +22,70 @@ app = Flask(__name__)
 
 
 @app.route("/")
-def index():
-    start_coords = (46.9540700, 142.7360300)
-    folium_map = folium.Map(location=start_coords, zoom_start=14)
-    return folium_map._repr_html_()
+def fullscreen():
+    """Simple example of a fullscreen map."""
+    m = folium.Map()
+    return m.get_root().render()
+
+
+@app.route("/iframe")
+def iframe():
+    """Embed a map as an iframe on a page."""
+    m = folium.Map()
+
+    # set the iframe width and height
+    m.get_root().width = "800px"
+    m.get_root().height = "600px"
+    iframe = m.get_root()._repr_html_()
+
+    return render_template_string(
+        """
+            <!DOCTYPE html>
+            <html>
+                <head></head>
+                <body>
+                    <h1>Using an iframe</h1>
+                    {{ iframe|safe }}
+                </body>
+            </html>
+        """,
+        iframe=iframe,
+    )
+
+
+@app.route("/components")
+def components():
+    """Extract map components and put those on a page."""
+    m = folium.Map(
+        width=800,
+        height=600,
+    )
+
+    m.get_root().render()
+    header = m.get_root().header.render()
+    body_html = m.get_root().html.render()
+    script = m.get_root().script.render()
+
+    return render_template_string(
+        """
+            <!DOCTYPE html>
+            <html>
+                <head>
+                    {{ header|safe }}
+                </head>
+                <body>
+                    <h1>Using components</h1>
+                    {{ body_html|safe }}
+                    <script>
+                        {{ script|safe }}
+                    </script>
+                </body>
+            </html>
+        """,
+        header=header,
+        body_html=body_html,
+        script=script,
+    )
 
 
 if __name__ == "__main__":
