afc3bca5d7411c8739c7448ddb3af8405d86c7b9
Frank <33519926+Conengmo@users.noreply.github.com>
2023-01-08 13:46:41 +0100

    New Sphinx theme

--- docs/conf.py
+++ docs/conf.py
@@ -102,7 +102,7 @@ pygments_style = "sphinx"
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = "f6"
+html_theme = "sphinx_rtd_theme"
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
@@ -110,7 +110,7 @@ html_theme = "f6"
 # html_theme_options = {}
 
 # Add any paths that contain custom themes here, relative to this directory.
-html_theme_path = ["_themes"]
+# html_theme_path = ["_themes"]
 
 # The name for this set of Sphinx documents.  If None, it defaults to
 # "<project> v<release> documentation".
