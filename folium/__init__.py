abf84758c55ba8e2559ad60607aab597de96fc68
Filipe Fernandes <ocefpaf@gmail.com>
2023-02-17 10:30:58 -0300

    fix lint and we don't need this b/c of min py requirement in setup.py

--- folium/__init__.py
+++ folium/__init__.py
@@ -1,4 +1,3 @@
-import sys
 
 import branca
 from branca.colormap import ColorMap, LinearColormap, StepColormap
@@ -59,28 +58,6 @@ if branca.__version__ != "unknown" and tuple(
     )
 
 
-if sys.version_info < (3, 0):
-    raise ImportError(
-        """You are running folium {} on Python 2
-
-    folium 0.9 and above are no longer compatible with Python 2, but somehow
-    you got this version anyway. Make sure you have pip >= 9.0 to avoid this
-    kind of issue, as well as setuptools >= 24.2:
-
-     $ pip install pip setuptools --upgrade
-
-    Your choices:
-
-    - Upgrade to Python 3.
-
-    - Install an older version of folium:
-
-     $ pip install 'folium<0.9.0'
-
-    """.format(
-            __version__
-        )
-    )  # noqa
 
 __all__ = [
     "Choropleth",
