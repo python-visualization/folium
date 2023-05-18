fb78f3e83f0b100eab8b2587509f683e43e3cf02
Frank <33519926+Conengmo@users.noreply.github.com>
2023-05-18 14:58:41 +0200

    more width

--- docs/conf.py
+++ docs/conf.py
@@ -133,6 +133,10 @@ html_theme = "pydata_sphinx_theme"
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ["_static"]
 
+html_css_files = [
+    "custom.css",
+]
+
 # If not '', a 'Last updated on:' timestamp is inserted at every page bottom,
 # using the given strftime format.
 # html_last_updated_fmt = '%b %d, %Y'
