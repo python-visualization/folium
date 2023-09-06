0d3dab247e4c8d148f26617b5b471159c28f23c3
Conengmo <33519926+Conengmo@users.noreply.github.com>
2023-09-06 12:25:27 +0200

    add logo to docs header

--- docs/conf.py
+++ docs/conf.py
@@ -129,7 +129,7 @@ html_title = "Folium documentation"
 
 # The name of an image file (relative to this directory) to place at the top
 # of the sidebar.
-# html_logo = None
+html_logo = "_static/folium_logo.png"
 
 # The name of an image file (within the static path) to use as favicon of the
 # docs.  This file should be a Windows icon file (.ico) being 16x16 or 32x32
