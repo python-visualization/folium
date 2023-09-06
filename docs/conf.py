57e8eaeb33e9a5a70f69130192bece66a6b6d070
Frank Anema <33519926+Conengmo@users.noreply.github.com>
2023-09-06 13:17:10 +0200

    Docs: folium logo (#1801)
    * Use transparent png logo
    
    * add logo to docs header
    
    * set logo height
    
    * set space between logo and header
    
    * logo text in header
    
    * Simpler index text, link to leaflet
    
    * typo

--- docs/conf.py
+++ docs/conf.py
@@ -123,6 +123,9 @@ html_theme_options = {
             "type": "fontawesome",
         }
     ],
+    "logo": {
+        "text": "Folium",
+    },
 }
 
 # Add any paths that contain custom themes here, relative to this directory.
@@ -130,14 +133,14 @@ html_theme_options = {
 
 # The name for this set of Sphinx documents.  If None, it defaults to
 # "<project> v<release> documentation".
-html_title = "Folium documentation"
+# html_title = "Folium documentation"
 
 # A shorter title for the navigation bar.  Default is the same as html_title.
 # html_short_title = None
 
 # The name of an image file (relative to this directory) to place at the top
 # of the sidebar.
-# html_logo = None
+html_logo = "_static/folium_logo.png"
 
 # The name of an image file (within the static path) to use as favicon of the
 # docs.  This file should be a Windows icon file (.ico) being 16x16 or 32x32
