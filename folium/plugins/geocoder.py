93753925488df78df7d5b0353b333a14380f4f19
prusswan <prusswan@gmail.com>
2024-01-10 20:16:03 +0800

    fixed import block (ruff --fix)

--- folium/plugins/geocoder.py
+++ folium/plugins/geocoder.py
@@ -1,10 +1,10 @@
+from typing import Optional
+
 from branca.element import MacroElement
 from jinja2 import Template
 
 from folium.elements import JSCSSMixin
 from folium.utilities import parse_options
-
-from typing import Optional
 
 
 class Geocoder(JSCSSMixin, MacroElement):
