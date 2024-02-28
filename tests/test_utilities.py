d9dfbf81f5af154dcb5ddf5f6ec53d823cbdbd68
Frank Anema <33519926+Conengmo@users.noreply.github.com>
2024-02-28 16:25:09 +0100

    Update tests/test_utilities.py

--- tests/test_utilities.py
+++ tests/test_utilities.py
@@ -219,7 +219,6 @@ def test_escape_double_quotes(text, result):
 def test_javascript_identifier_path_to_array_notation(text, result):
     assert javascript_identifier_path_to_array_notation(text) == result
 
-    
 def test_js_code_init_str():
     js_code = JsCode("hi")
     assert isinstance(js_code, JsCode)
