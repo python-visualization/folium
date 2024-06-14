20c1676318ee043973fb594028eb0995657d291b
ocefpaf <ocefpaf@users.noreply.github.com>
2024-06-14 10:22:24 +0000

    deploy: 2921126e0814267009fae45ddb3a683bedf37db1

--- latest/_static/documentation_options.js
+++ latest/_static/documentation_options.js
@@ -1,5 +1,5 @@
 const DOCUMENTATION_OPTIONS = {
-    VERSION: '0.16.1.dev74+g67579062',
+    VERSION: '0.16.1.dev76+g2921126e',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
