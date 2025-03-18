7cf5314556c7baf70707e4b085e787e9785e7121
Conengmo <Conengmo@users.noreply.github.com>
2025-03-18 08:01:52 +0000

    deploy: a559a9f36b94486ef9aa97ed5d579b5143408343

--- dev/_static/documentation_options.js
+++ dev/_static/documentation_options.js
@@ -1,5 +1,5 @@
 const DOCUMENTATION_OPTIONS = {
-    VERSION: '0.19.6.dev13+g661fc9f1',
+    VERSION: '0.19.6.dev14+ga559a9f3',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
