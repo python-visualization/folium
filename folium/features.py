74dadcbea75b8144acf7e338e4164d95241c1dcd
Hans Then <hans.then@gmail.com>
2025-04-06 19:21:03 +0200

    Add leaflet control

--- folium/features.py
+++ folium/features.py
@@ -7,7 +7,17 @@ import functools
 import json
 import operator
 import warnings
-from typing import Any, Callable, Dict, Iterable, List, Optional, Sequence, Tuple, Union
+from typing import (
+    Any,
+    Callable,
+    Dict,
+    Iterable,
+    List,
+    Optional,
+    Sequence,
+    Tuple,
+    Union,
+)
 
 import numpy as np
 import requests
@@ -1992,3 +2002,54 @@ class ColorLine(FeatureGroup):
             out.setdefault(cm(color), []).append([[lat1, lng1], [lat2, lng2]])
         for key, val in out.items():
             self.add_child(PolyLine(val, color=key, weight=weight, opacity=opacity))
+
+
+class Control(JSCSSMixin, MacroElement):
+    """
+    Add a Leaflet Control object to the map
+
+    Parameters
+    ----------
+    control: str
+        The javascript class name of the control to be rendered.
+    position: str
+        One of "bottomright", "bottomleft", "topright", "topleft"
+
+    Examples
+    --------
+
+    >>> import folium
+    >>> from folium.features import Control, Marker
+    >>> from folium.plugins import Geocoder
+
+    >>> m = folium.Map(
+    ...     location=[46.603354, 1.8883335], attr=None, zoom_control=False, zoom_start=5
+    ... )
+    >>> Control("Zoom", position="topleft").add_to(m)
+    """
+
+    _template = Template(
+        """
+      {% macro script(this, kwargs) %}
+          var {{ this.get_name() }} = new L.Control.{{this._name}}(
+              {% for arg in this.args %}
+                  {{ arg | tojavascript }},
+              {% endfor %}
+              {{ this.options|tojavascript }}
+          ).addTo({{ this._parent.get_name() }});
+      {% endmacro %}
+    """
+    )
+
+    def __init__(
+        self,
+        control: Optional[str] = None,
+        *args,
+        **kwargs,
+    ):
+        super().__init__()
+        if control:
+            self._name = control
+
+        self.args = args
+        self.options = remove_empty(**kwargs)
