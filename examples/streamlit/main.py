758e8486991c7bd7a005ea6d65015db015ccd735
Hans Then <hans.then@gmail.com>
2025-04-07 18:54:45 +0200

    Add sample playwright tests for folium
    First try: just do the draw plugin.

---
+++ examples/streamlit/main.py
@@ -0,0 +1,11 @@
+import streamlit as st
+
+st.logo("https://python-visualization.github.io/folium/latest/_static/folium_logo.png")
+st.title("Python data, leaflet.js maps")
+
+"""
+Folium builds on the data wrangling strengths of the Python ecosystem
+and the mapping strengths of the Leaflet.js library.
+
+Manipulate your data in Python, then visualize it in a Leaflet map via
+Folium."""
