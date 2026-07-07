a6c72971dbc666ad826040fc1465dbf7e257fe40
hansthen <hansthen@users.noreply.github.com>
2026-07-07 15:48:20 +0000

    deploy: f15eff39f335a31c0688282a94a3a7118af84513

--- dev/_static/documentation_options.js
+++ dev/_static/documentation_options.js
@@ -1,5 +1,5 @@
 const DOCUMENTATION_OPTIONS = {
-    VERSION: '0.20.1.dev139+ge17b64c58',
+    VERSION: '0.20.1.dev141+gf15eff39f',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
